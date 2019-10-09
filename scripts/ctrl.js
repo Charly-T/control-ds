ef98832462120607f7028009502220a79f7155db
set.charly@gmail.com <set.charly@gmail.com>
2019-10-09 19:44:40 +0200

    fix: updated loading entrypoint

--- scripts/ctrl.js
+++ scripts/ctrl.js
@@ -1,7 +1,7 @@
-import * as theme from './theme.js';
-import * as button from './button.js';
-import * as tabs from './tabs.js';
-import * as tabsBar from './tabsBar.js';
-import * as tab from './tab.js';
-import * as tabsContent from './tabsContent.js';
-import * as tabContent from './tabContent.js';
+import './theme.js';
+import './button.js';
+import './tabs.js';
+import './tabsBar.js';
+import './tab.js';
+import './tabsContent.js';
+import './tabContent.js';
